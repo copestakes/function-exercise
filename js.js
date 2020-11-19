3dac30a724506d4aa6d368e8729b4023460bdce6
David Copestakes <davecopestakes@gmail.com>
2020-11-19 09:20:46 -0500

    Initial commit

---
+++ js.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    console.log('hello ' + name);
+}
+
+greet('Dave');
+
+function area(width, height) {
+    let total = (width * height);
+    console.log(total);
+}
+area(3,5);
